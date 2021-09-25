031f7c77af39ccb2fd9b23a88f1eb071df606eff
Roman <56370973+RomaTocks@users.noreply.github.com>
2021-09-25 23:35:00 +0300

    Added the ability to change the language.

---
+++ test/src/main/resources/static/js/languages.js
@@ -0,0 +1,65 @@
+const languagesList = ['ru', 'en'];
+let downloadedLanguagesCount = 0;
+const languages = {};
+let currentLang;
+
+loadLocalLanguage();
+loadJSON();
+
+function setLanguage(element) {
+    currentLang = element.value;
+    localStorage.setItem("language", element.value);
+    changeLanguage();
+}
+function changeLanguage() {
+    document.body.setAttribute("lang",currentLang)
+    const allTranslatedFields = document.querySelectorAll("[data-i18n]");
+    for (let field of allTranslatedFields) {
+        const wordKey = field.getAttribute("data-i18n");
+        const translatedWord = languages[currentLang][wordKey].message;
+        if(field.tagName === "INPUT") field.value = translatedWord;
+        else field.innerText = translatedWord;
+    }
+}
+function languagesReady() {
+    if (++downloadedLanguagesCount < languagesList.length) return;
+    let buttons = document.getElementById("lang-buttons");
+    buttons.className = "form_toggle";
+    changeLanguage();
+}
+function loadLocalLanguage() {
+    let localLang = localStorage.getItem("language")
+    if( localLang !== null) {
+        currentLang = localLang;
+        updateLanguagesButtons();
+    }
+    else {
+        localStorage.setItem("language", 'ru')
+        currentLang = 'ru';
+        updateLanguagesButtons();
+    }
+}
+function updateLanguagesButtons() {
+    let buttons = document.getElementsByClassName("language");
+    let currentButton;
+    for (let button of buttons) {
+        button.removeAttribute("checked");
+        if(button.value === currentLang) {
+            currentButton = button;
+        }
+    }
+    currentButton.setAttribute("checked",true);
+}
+function loadJSON() {
+    languagesList.forEach(lang => {
+        fetch('http://localhost:8080/_locales/'+ lang +'/' + lang + '.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then(data => {
+                    languages[lang] = data;
+                    languagesReady();
+                }
+            );
+    });
+}
--- test/src/main/resources/static/js/orderlist.js
+++ test/src/main/resources/static/js/orderlist.js
@@ -2,27 +2,39 @@ let nameException = document.getElementById("name-exception");
 nameException.innerText = "";
 function handleSubmit() {
     let answer = true;
-    let nameAnswer = validation("ФИО","name");
-    let addressAnswer= validation("Адрес","address");
+    let nameAnswer = validation("name");
+    let addressAnswer= validation("address");
     if(!nameAnswer || !addressAnswer){
         answer = false;
     }
     return answer;
 }
-function validation(name, id) {
+function validation(id) {
     let answer = true;
-    let element = document.getElementById(id);
+    let input = document.getElementById(id);
+    let label = document.getElementById(id + "-label")
     let exception = document.getElementById(id + "-exception");
-    element.classList.remove("error");
+    let lang = document.body.lang;
+    input.classList.remove("error");
     exception.innerText="";
-    let field = element.value.trim();
+    let field = input.value.trim();
     if(field === "") {
-        exception.innerText = name + " не может быть пустым или содержать пробелы!";
-        answer = false;
+        if(lang === 'ru') {
+            exception.innerText = label.innerText + " не может быть пустым или содержать пробелы!";
+        }
+        else {
+            exception.innerText = label.innerText + " can't be empty or keep only spaces!";
+        }
+        answer = false
     }
     if (field.length < 2 || field.length >= 15) {
         document.getElementById(id).classList.add("error");
-        exception.innerText = name + " должно быть больше 2 символов и меньше 15!";
+        if(lang === 'ru') {
+            exception.innerText = label.innerText + " должно быть больше 2 символов и меньше 15!";
+        }
+        else {
+            exception.innerText = label.innerText + " must be more than 2 symbols and less than 15 symbols!";
+        }
         answer = false;
     }
     return answer;
