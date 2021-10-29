26f7ded76ca086007d17a98f6c6f23d0f7ab45fe
Roman <56370973+RomaTocks@users.noreply.github.com>
2021-10-29 23:39:13 +0300

    Added navbar and fixed mobile adaptivity.

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -1,14 +1,31 @@
+let openness = false;
+let overlay = document.getElementById("overlay");
+let burger = document.getElementById("burger");
+let sidebar = document.getElementById("sidebar");
+let sidebarElements = document.getElementsByClassName("sidebar-element");
+let groupElement = document.getElementById("sidebar-group");
+window.onresize = function (e) {
+    if(e.target.innerWidth > 767 && openness) {
+        navbar();
+    }
+}
 function navbar() {
-    let burger = document.getElementById("burger");
+
     burger.classList.toggle("burger-active");
-    let sidebar = document.getElementById("sidebar");
     if (sidebar.classList.contains("sidebar-active")) {
+        getOverlay().className = "overlay-not-active";
+        document.body.style.overflow = "visible";
         sidebar.className = "sidebar-not-active";
+        openness = false;
+        getOverlay().remove();
     }
     else {
+        createOverlay();
+        getOverlay().className = "overlay-active";
+        document.body.style.overflow = "hidden";
         sidebar.className = "sidebar-active";
+        openness = true;
     }
-    let sidebarElements = document.getElementsByClassName("sidebar-element");
     for (let sidebarElement of sidebarElements) {
         if (sidebarElement.classList.contains("element-active")) {
             sidebarElement.classList.remove("element-active");
@@ -19,7 +36,6 @@ function navbar() {
             sidebarElement.classList.add("element-active");
         }
     }
-    let groupElement = document.getElementById("sidebar-group");
     if(groupElement.classList.contains("right-group-active")) {
         groupElement.classList.remove("right-group-active");
         groupElement.classList.add("right-group-not-active");
@@ -28,4 +44,13 @@ function navbar() {
         groupElement.classList.remove("right-group-not-active");
         groupElement.classList.add("right-group-active");
     }
+}
+function getOverlay() {
+    return document.getElementById("overlay");
+}
+function createOverlay() {
+    let overlayBlock = document.createElement("div");
+    overlayBlock.id = "overlay";
+    overlayBlock.className = "overlay-not-active";
+    document.getElementById("main").after(overlayBlock);
 }
